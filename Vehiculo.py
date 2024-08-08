b304b5e70dc71633dbfcad5408df8546addd130b
ocamilavillero09 <111584990+ocamilavillero09@users.noreply.github.com>
2024-08-08 16:45:56 -0500

    Actualización punto 2.py

--- Vehiculo.py
+++ Vehiculo.py
@@ -1,3 +1,4 @@
+#punto 1
 class Vehiculo:
     def __init__(self, velocidadmax, kilometraje):
         self.velocidadmax = velocidadmax
@@ -5,6 +6,24 @@ class Vehiculo:
 #Aqui verificamos que la clase creada esta correcta     
 #vehiculo = Vehiculo(100, 10000)            
 #print(vehiculo.kilometraje)
+
+#punto2
+import math
+class Punto:
+    def __init__(self,x,y,distancia):
+        self.x = x
+        self.y = y
+        self.distancia= distancia
+        
+    def mostar(self,x,y):
+        print (x,y)
+        
+    def mover(self,nueva_x,nueva_y):
+        self.x = nueva_x
+        self.y = nueva_y
+        
+    def calcular_distancia(self,nueva_x,nueva_y,z,p):
+        self.distancia = math.sqrt((nueva_x-nueva_y)**2+(z-p)**2)
         
 
         
