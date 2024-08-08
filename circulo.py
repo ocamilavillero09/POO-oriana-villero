163e7fe290f3abb4a5e5795d47bb877515c2f865
ocamilavillero09 <111584990+ocamilavillero09@users.noreply.github.com>
2024-08-08 14:06:50 -0500

    correcciones circulo.py

--- circulo.py
+++ circulo.py
@@ -1,15 +1,19 @@
 import math
+
 class Circulo:
-    def __inint__(self, centro, radio,area, perimetro):
-        self_centro = centro 
-        self_radio = radio
-        self_area = area
-        self_perimetro = perimetro
-        
-    def area (self, radio):
-        self_area = math.pi * radio ** 2
+    def __init__(self, centro, radio):
+        self.centro = centro 
+        self.radio = radio
     
-    def perimetro (self, radio):
-        self_perimetro = 2*math.pi*radio   
+    def area(self,radio):
+        self.area = math.pi * self.radio ** 2
+    
+    def perimetro(self,radio):
+        self.perimetro =  2 * math.pi * self.radio   
+    
+    def punto_pertenece(self, punto):
+        distancia_al_centro = math.sqrt((punto[0] - self.centro[0]) ** 2 + (punto[1] - self.centro[1]) ** 2)
+        return distancia_al_centro <= self.radio
+
         
     
