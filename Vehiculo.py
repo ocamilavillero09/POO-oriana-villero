a47fbf0dd8e4b474c70dcc4a1ebcbb34e198d392
ocamilavillero09 <111584990+ocamilavillero09@users.noreply.github.com>
2024-08-08 16:50:57 -0500

    punto 5.py

--- Vehiculo.py
+++ Vehiculo.py
@@ -40,6 +40,22 @@ class Punto:
         
         #def calcular_area(self,p1,p2):
             #self.area=  
+
+#punto 5
+    class Circulo:
+    def __init__(self, centro, radio):
+        self.centro = centro 
+        self.radio = radio
+    
+    def area(self,radio):
+        self.area = math.pi * self.radio ** 2
+    
+    def perimetro(self,radio):
+        self.perimetro =  2 * math.pi * self.radio   
+    
+    def punto_pertenece(self, punto):
+        distancia_al_centro = math.sqrt((punto[0] - self.centro[0]) ** 2 + (punto[1] - self.centro[1]) ** 2)
+        return distancia_al_centro <= self.radio
         
 
         
